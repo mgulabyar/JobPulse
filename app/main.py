aa999e8e2ee0b35f6d302d287049b76108004bdb
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-04 15:11:01 +0500

    refactor: improve app startup flow

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.core.config import PROJECT_NAME
 from app.routers.health import router as health_router
-# main file 
+# 
 app = FastAPI(title=PROJECT_NAME)
 
 app.include_router(health_router, prefix="/api")
