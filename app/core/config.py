bc6ef9d3fa546bcf00fcfb075a0db8b6748215ff
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-04 15:14:05 +0500

    chore: remove tracked cache files

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
+# name name from .env file or default to "JobPulse"
 PROJECT_NAME = os.getenv("PROJECT_NAME", "JobPulse")
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "jobpulse")
