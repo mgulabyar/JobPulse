0040cf46e5de61e4690d76c74ddf802e9a914fa6
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-04 15:15:02 +0500

    refactor: improve folder naming

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# name
 PROJECT_NAME = os.getenv("PROJECT_NAME", "JobPulse")
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "jobpulse")
